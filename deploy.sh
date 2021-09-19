f49b46b6b1f965e72a1a6d3cbc0c85bb94263191
lmaosoggypancakes <lmaosoggywaffles@gmail.com>
2021-09-19 14:37:41 -0700

    okoko

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+npm run build
+echo "-----------------------------------\nVue project compiled\n-----------------------------------"
 git add .
 echo "Git Commit Message: "
 read msg
@@ -8,3 +10,12 @@ git push --set-upstream origin master
 clear
 
 echo "-----------------------------------\nPushed project to repo\n-----------------------------------"
+
+
+cd dist
+git init
+git add .
+git commit -m "$msg"
+git push --set-upstream https://github.com/lmaosoggypancakes/lmaosoggypancakes.github.io gh-pages 
+rm -rf .git
+clear
