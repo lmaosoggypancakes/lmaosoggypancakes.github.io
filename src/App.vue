331ac6a50306e075fc1adf5c0e639a484f64731e
lmaosoggypancakes <lmaosoggywaffles@gmail.com>
2021-09-19 13:40:35 -0700

    hello xd

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+import Vue from 'vue'
+export default Vue.extend({
+})
+</script>
+<style lang="sass">
+@import "./variables"
+body 
+  margin: 0
+  padding: 0
+  font-family: "Ubuntu", sans-serif
+  overflow-x: hidden
+p
+  padding: 0
+  margin: 0
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,173 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="blue" v-observe-visibility="{
+      callback: (isVisible, entry) => introduce(isVisible, entry, 'blue'),
+      once: true,
+    }">
+      <p :style="{
+      'animation-name': blue,
+      'animation-fill-mode': 'forwards',
+      'animation-duration': '1s',
+      'animation-delay': '0.5s'
+      
+    }">   
+        Hi! 👋 I'm Josef.
+      </p>
+    </div>
+    <div class="spacer layer1"></div>
+    <div class="orange" v-observe-visibility="{
+      callback: (isVisible, entry) => introduce(isVisible, entry, 'orange'),
+      once: true,
+    }">
+      <p :style="{
+      'animation-name': orange,
+      'animation-fill-mode': 'forwards',
+      'animation-duration': '1s',
+      'animation-delay': '0.5s'
+      
+    }">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae doloribus temporibus sed earum. Nam laboriosam fuga eveniet dolores necessitatibus veritatis, incidunt provident iure, quas impedit amet recusandae praesentium, soluta enim.
+      </p>
+    </div>
+    <div class="spacer layer2">
+    </div>
+    <div class="lightblue" v-observe-visibility="{
+      callback: (isVisible, entry) => introduce(isVisible, entry, 'lightblue'),
+      once: true,
+    }">
+      <p :style="{
+      'animation-name': lightblue,
+      'animation-fill-mode': 'forwards',
+      'animation-duration': '1s',
+      'animation-delay': '0.5s'
+      
+    }">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus possimus doloribus doloremque maiores provident porro nostrum consequatur consectetur impedit nemo quod tempore eius tenetur sapiente sit quia voluptate, voluptatibus distinctio.
+      </p>
+    </div>
+    <div class="spacer layer3"></div>
+    <div class="darkblue" v-observe-visibility="{
+      callback: (isVisible, entry) => introduce(isVisible, entry, 'darkblue'),
+      once: true,
+    }">
+      <p :style="{
+      'animation-name': darkblue,
+      'animation-fill-mode': 'forwards',
+      'animation-duration': '1s',
+      'animation-delay': '0.5s'
+      
+    }">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit sequi neque perspiciatis quaerat adipisci nemo omnis, fuga optio et mollitia obcaecati reprehenderit laudantium quae, eligendi qui, odit nobis ut dicta.
+      </p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
 export default Vue.extend({
   name: "Home",
   components: {
-    HelloWorld,
   },
-});
+  data() {
+    return {
+      blue: "",
+      orange: "",
+      lightblue: "",
+      darkblue: ""
+    }
+  },
+  methods: {
+    introduce(e: boolean, el: any, b: string) {
+      if (e) {
+        console.log(`${b} is visible`)
+        this[b] = "intro"
+      }
+    }
+  }
+})
 </script>
+
+<style lang="sass">
+p
+  opacity: 0
+.blue
+  background-color: #3d5a80
+  width: 100vw
+  height: 70vh
+  color: white
+  font-size: 2em
+  display: flex
+  align-items: center
+  justify-content: center
+  p 
+    max-width: 60%
+.lightblue
+  background-color: #98C1D9
+  width: 100vw
+  height: 40vh
+  color: black
+  font-size: 2em
+  display: flex
+  align-items: center
+  justify-content: center
+  p 
+    max-width: 60%
+.white
+  background-color: #e0fbfc
+  width: 100vw
+  height: 40vh
+  color: black
+  font-size: 2em
+  display: flex
+  align-items: center
+  justify-content: center
+  p 
+    max-width: 60%
+.orange 
+  background-color: #ee6c4d
+  width: 100vw
+  height: 40vh
+  color: white
+  font-size: 2em
+  display: flex
+  align-items: center
+  justify-content: center
+  p 
+    max-width: 60%
+.darkblue
+  background-color: #293241
+  width: 100vw
+  height: 40vh
+  color: white
+  font-size: 2em
+  display: flex
+  align-items: center
+  justify-content: center
+  p 
+    max-width: 60%
+.spacer
+  aspect-ratio : 960/300
+  width: 100%
+  background-repeat: no-repeat
+  background-position: center
+  background-size: cover
+.layer1
+  background-image: url("../assets/t1.svg")
+.layer2
+  background-image: url("../assets/t2.svg")
+.layer3 
+  background-image: url("../assets/t3.svg")
+
+
+
+// animations
+
+
+@keyframes intro 
+  from 
+    opacity: 0
+  to 
+    opacity: 1
+
+</style>
